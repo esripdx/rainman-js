a5ec50f681d80d3d081fb14dc0278a63ae6e9d82
Jerry Sievert <code@legitimatesounding.com>
2014-04-30 14:54:07 -0700

    initial import

---
+++ index.js
@@ -0,0 +1,50 @@
+var request = require('request');
+
+
+function report (endpoint, group_id, key, value, number, callback, optionalData) {
+  optionalData = optionalData || { };
+
+  var date = optionalData.date || new Date();
+  var precision = optionalData.precision || 'day';
+
+  var formData = {
+    group_id: group_id,
+    client_id: optionalData.client_id,
+    key: key,
+    value: value,
+    number: number,
+    date: dateToFormat(date, precision)
+  };
+
+  request.post(endpoint, function (err, body, response) {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null);
+    }
+  }).form(formData);
+}
+
+
+function pad(str, length) {
+  str = String(str);
+  while (str.length < length) {
+    str = '0' + str;
+  }
+  return str;
+}
+
+function dateToFormat (date, precision) {
+  var month = pad(date.getMonth() + 1, 2);
+  var day = pad(date.getDate(), 2);
+
+  var output = date.getFullYear() + "-" + month + "-" + day;
+
+  if (precision === 'hour') {
+    output += " " + pad(date.getHours(), 2);
+  }
+
+  return output;
+}
+
+exports.report = report;
